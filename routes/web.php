15ec363aaa8fe6af9710e3f2e9f2699cadd4f420
nakajima reina <boofoowoo.15@gmail.com>
2021-11-25 04:57:16 +0000

    PHP12

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,15 @@ Route::group(['prefix'=> 'admin'],function(){
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create','Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\NewsController@add')->middleware('auth');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
